41a6667a67681016655f5cbc6b71bdd5897cb9aa
Yasir N.Romaya <yasserdghx@gmail.com>
2020-10-22 13:42:23 +0300

    Initial project setup

---
+++ DXLib/DXLib.h
@@ -0,0 +1,18 @@
+//
+//  DXLib.h
+//  DXLib
+//
+//  Created by Yasir N.Ramaya on 10/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DXLib.
+FOUNDATION_EXPORT double DXLibVersionNumber;
+
+//! Project version string for DXLib.
+FOUNDATION_EXPORT const unsigned char DXLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DXLib/PublicHeader.h>
+
+
---
+++ DXLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DXLibExamples
+//
+//  Created by Yasir N.Ramaya on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
