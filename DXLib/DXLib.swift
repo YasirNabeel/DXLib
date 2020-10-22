eaefb6a4511113e5d54950f49b6c6297f47ddc72
Yasir N.Romaya <yasserdghx@gmail.com>
2020-10-22 14:03:34 +0300

    Added test cases

---
+++ DXLib/DXLib.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftyLib.swift
+//
+
+
+public final class DXLib {
+
+    let name = "DXLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
